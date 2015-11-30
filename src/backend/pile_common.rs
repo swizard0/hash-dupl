a1eb77536feb13de0442a9c3c705765c278cf532
Alexey Voznyuk <me@swizard.info>
2015-11-30 15:11:43 +0300

    implement serialize/deserialize for State

--- src/backend/pile_common.rs
+++ src/backend/pile_common.rs
@@ -1,8 +1,10 @@
 use std::marker::PhantomData;
 use std::cmp::{PartialEq, PartialOrd, Ordering};
-use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use time::Timespec;
+use serde::{de, Serialize, Serializer, Deserialize, Deserializer};
 use bin_merge_pile::reduce::Reducer;
 use slices_merger::SlicesMerger;
+use super::super::{Config, State};
 
 pub struct BandEntry {
     pub band: u64,
@@ -31,6 +33,54 @@ impl Deserialize for BandEntry {
     fn deserialize<D>(deserializer: &mut D) -> Result<BandEntry, D::Error> where D: Deserializer {
         let (band, docs) = try!(Deserialize::deserialize(deserializer));
         Ok(BandEntry { band: band, docs: docs, })
+    }
+}
+
+const STATE_MAGIC: u64 = 0xb5e05a82649d271f;
+
+impl Serialize for State {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error> where S: Serializer {
+        try!(STATE_MAGIC.serialize(serializer));
+        try!(self.config.shingle_length.serialize(serializer));
+        try!(self.config.signature_length.serialize(serializer));
+        try!(self.config.similarity_threshold.serialize(serializer));
+        try!(self.config.band_min_probability.serialize(serializer));
+        try!(self.created_at.sec.serialize(serializer));
+        try!(self.created_at.nsec.serialize(serializer));
+        try!(self.band_length.serialize(serializer));
+        try!(self.minhash_seeds.serialize(serializer));
+        try!(self.bands_seeds.serialize(serializer));
+        Ok(())
+    }
+}
+
+impl Deserialize for State {
+    fn deserialize<D>(deserializer: &mut D) -> Result<State, D::Error> where D: Deserializer {
+        let magic: u64 = try!(Deserialize::deserialize(deserializer));
+        if magic != STATE_MAGIC {
+            return Err(de::Error::syntax(&format!("invalid state magic: {}, expected: {}", magic, STATE_MAGIC)))
+        }
+        let shingle_length = try!(Deserialize::deserialize(deserializer));
+        let signature_length = try!(Deserialize::deserialize(deserializer));
+        let similarity_threshold = try!(Deserialize::deserialize(deserializer));
+        let band_min_probability = try!(Deserialize::deserialize(deserializer));
+        let sec = try!(Deserialize::deserialize(deserializer));
+        let nsec = try!(Deserialize::deserialize(deserializer));
+        let band_length = try!(Deserialize::deserialize(deserializer));
+        let minhash_seeds = try!(Deserialize::deserialize(deserializer));
+        let bands_seeds = try!(Deserialize::deserialize(deserializer));
+        Ok(State {
+            config: Config {
+                shingle_length: shingle_length,
+                signature_length: signature_length,
+                similarity_threshold: similarity_threshold,
+                band_min_probability: band_min_probability,
+            },
+            created_at: Timespec { sec: sec, nsec: nsec, },
+            band_length: band_length,
+            minhash_seeds: minhash_seeds,
+            bands_seeds: bands_seeds,
+        })
     }
 }
 
