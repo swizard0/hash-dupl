0af2e4bfbb493712f3077db73398cf5f7028471e
Alexey Voznyuk <me@swizard.info>
2015-12-01 13:26:22 +0300

    partially implemented (yet without rotate)

--- src/backend/stream.rs
+++ src/backend/stream.rs
@@ -17,12 +17,15 @@ pub enum Error {
     ReadWindowsDir(PathBuf, io::Error),
     ReadWindowsDirEntry(PathBuf, io::Error),
     StatWindowsDirEntry(PathBuf, io::Error),
+    StatWindowRWDir(PathBuf, io::Error),
     PileRw(pile_rw::Error),
     PileLookup(pile_lookup::Error),
     PileCompile(pile_compile::Error),
     MissingStateForWindow(PathBuf),
     WindowsStatesInconsistency(PathBuf),
     UnsupportedForROWindow,
+    WindowRWNotCreated,
+    StateIsAlreadySaved,
 }
 
 impl From<pile_rw::Error> for Error {
@@ -48,6 +51,7 @@ enum Window<D> where D: Serialize + Deserialize + Send + Sync + 'static {
 }
 
 pub struct Stream<D> where D: Serialize + Deserialize + Send + Sync + 'static {
+    state: Option<Arc<State>>,
     windows_dir: PathBuf,
     windows: Vec<Window<D>>,
     rw_window: Option<InnerWindow<Worker<pile_rw::PileRw<D>, Error>>>,
@@ -94,6 +98,7 @@ impl<D> Stream<D> where D: Serialize + Deserialize + Send + Sync + 'static {
         }
 
         let mut stream = Stream {
+            state: None,
             windows_dir: base_dir,
             windows: windows,
             rw_window: None,
@@ -106,7 +111,7 @@ impl<D> Stream<D> where D: Serialize + Deserialize + Send + Sync + 'static {
     }
 
     fn create_rw_window(&mut self) -> Result<(), Error> {
-        let mut common_state: Option<Arc<State>> = None;
+        let mut common_state = self.state.as_ref().map(|s| s.clone());
         for win in self.windows.iter_mut() {
             let (database_dir, maybe_state) = match win {
                 &mut Window::Rw(InnerWindow { database_dir: ref dir, backend: ref mut win, }) => (dir, try!(win.load_state())),
@@ -125,19 +130,217 @@ impl<D> Stream<D> where D: Serialize + Deserialize + Send + Sync + 'static {
             }
         }
 
-        let now = time::now_utc();
-        let mut database_dir = PathBuf::new();
-        database_dir.push(&self.windows_dir);
-        database_dir.push(format!("{}", now.rfc3339()));
+        let mut database_dir;
+        let mut index = 0;
+        loop {
+            let now = time::now_utc();
+            database_dir = PathBuf::new();
+            database_dir.push(&self.windows_dir);
+            database_dir.push(format!("{}_{}", now.rfc3339(), index));
+            match fs::metadata(&database_dir) {
+                Ok(..) => (),
+                Err(ref e) if e.kind() == io::ErrorKind::NotFound => break,
+                Err(e) => return Err(Error::StatWindowRWDir(database_dir, e)),
+            }
+            index += 1;
+        }
 
         let mut pile_rw = try!(pile_rw::PileRw::new(&database_dir, self.compile_params.clone()));
         if let Some(state) = common_state.take() {
-            try!(pile_rw.save_state(state));
+            try!(pile_rw.save_state(state.clone()));
+            if self.state.is_none() {
+                self.state = Some(state)
+            }
         }
         self.rw_window = Some(InnerWindow {
             database_dir: database_dir,
             backend: Worker::run_redirect_lookup(pile_rw, self.lookup_tx.clone()),
         });
         Ok(())
+    }
+}
+
+impl<D> Backend for Stream<D> where D: Serialize + Deserialize + Send + Sync + 'static {
+    type Error = Error;
+    type Document = D;
+
+    fn save_state(&mut self, state: Arc<State>) -> Result<(), Error> {
+        if self.state.is_none() {
+            if let Some(InnerWindow { backend: ref mut bkd, ..}) = self.rw_window {
+                self.state = Some(state.clone());
+                Ok(try!(bkd.save_state(state)))
+            } else {
+                Err(Error::WindowRWNotCreated)
+            }
+        } else {
+            Err(Error::StateIsAlreadySaved)
+        }
+    }
+
+    fn load_state(&mut self) -> Result<Option<Arc<State>>, Error> {
+        Ok(self.state.as_ref().map(|s| s.clone()))
+    }
+
+    fn insert(&mut self, signature: Arc<Signature>, doc: Arc<D>) -> Result<(), Error> {
+        if let Some(InnerWindow { backend: ref mut bkd, ..}) = self.rw_window {
+            Ok(try!(bkd.insert(signature, doc)))
+        } else {
+            Err(Error::WindowRWNotCreated)
+        }
+    }
+
+    fn lookup<F, C, CR, CE>(&mut self, signature: Arc<Signature>, filter: F, mut collector: C) -> Result<CR, LookupError<Error, CE>>
+        where F: CandidatesFilter + Clone, C: CandidatesCollector<Error = CE, Document = D, Result = CR>
+    {
+        let boxed_filter = Box::new(filter);
+        let mut waiting_replies = 0;
+        for win in self.windows.iter() {
+            match win {
+                &Window::Rw(InnerWindow { backend: ref win, .. }) =>
+                    win.tx.send(Req::Lookup(signature.clone(), boxed_filter.clone())).unwrap(),
+                &Window::Ro(InnerWindow { backend: ref win, .. }) =>
+                    win.tx.send(Req::Lookup(signature.clone(), boxed_filter.clone())).unwrap(),
+            }
+            waiting_replies += 1;
+        }
+        if let Some(InnerWindow { backend: ref win, ..}) = self.rw_window {
+            win.tx.send(Req::Lookup(signature.clone(), boxed_filter.clone())).unwrap();
+            waiting_replies += 1;
+        }
+
+        let mut error_occurred = None;
+        while waiting_replies > 0 {
+            match self.lookup_rx.recv() {
+                Ok(Ok(Rep::LookupResult(similarity, doc))) =>
+                    try!(collector.receive(similarity, doc).map_err(|e| LookupError::Collector(e))),
+                Ok(Ok(Rep::LookupFinish)) =>
+                    waiting_replies -= 1,
+                Ok(Err(e)) => {
+                    waiting_replies -= 1;
+                    error_occurred = Some(LookupError::Backend(e));
+                },
+                other =>
+                    panic!("unexpected rep: {:?}", other),
+            }
+        }
+
+        if let Some(error) = error_occurred {
+            Err(error)
+        } else {
+            collector.finish().map_err(|e| LookupError::Collector(e))
+        }
+    }
+
+    fn rotate(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use std::sync::Arc;
+    use bin_merge_pile::merge::ParallelConfig;
+    use super::Stream;
+    use super::super::pile_compile::Params;
+    use super::super::super::{Backend, SimilarityThresholdFilter, Signature, State, Config};
+
+    #[test]
+    fn save_load_state() {
+        let _ = fs::remove_dir_all("/tmp/stream_a");
+        let state = Arc::new(State::new(Config::default()));
+        let mut backend = Stream::<String>::new("/tmp/stream_a", Params {
+            min_tree_height: 1,
+            max_block_size: 32,
+            memory_limit_power: 13,
+            parallel_config: ParallelConfig::SingleThread,
+        }).unwrap();
+        assert_eq!(backend.load_state().unwrap(), None);
+        backend.save_state(state.clone()).unwrap();
+        assert_eq!(backend.load_state().unwrap().unwrap(), state.clone());
+    }
+
+    #[test]
+    fn insert_lookup_rotate() {
+        let doc_a = Arc::new("some text".to_owned());
+        let doc_b = Arc::new("some other text".to_owned());
+        {
+            let _ = fs::remove_dir_all("/tmp/stream_b");
+            let mut backend = Stream::<String>::new("/tmp/stream_b", Params {
+                min_tree_height: 1,
+                max_block_size: 32,
+                memory_limit_power: 13,
+                parallel_config: ParallelConfig::SingleThread,
+            }).unwrap();
+            backend.save_state(Arc::new(State::new(Config::default()))).unwrap();
+            backend.insert(Arc::new(Signature { minhash: vec![1, 2, 3], bands: vec![100, 300, 400], }), doc_a.clone()).unwrap();
+            backend.insert(Arc::new(Signature { minhash: vec![4, 5, 6], bands: vec![200, 300, 500], }), doc_b.clone()).unwrap();
+
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![1, 2, 3], bands: vec![100, 400], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 1);
+            assert_eq!(results[0].similarity, 1.0);
+            assert_eq!(results[0].document, doc_a.clone());
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![4, 5, 6], bands: vec![200, 500, 600, 700], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 1);
+            assert_eq!(results[0].similarity, 1.0);
+            assert_eq!(results[0].document, doc_b.clone());
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![1, 2, 4], bands: vec![300], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 2);
+            assert_eq!(results[0].document, doc_a.clone());
+            assert_eq!(results[1].document, doc_b.clone());
+
+            backend.rotate().unwrap();
+
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![1, 2, 3], bands: vec![100, 400], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 1);
+            assert_eq!(results[0].similarity, 1.0);
+            assert_eq!(results[0].document, doc_a.clone());
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![4, 5, 6], bands: vec![200, 500, 600, 700], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 1);
+            assert_eq!(results[0].similarity, 1.0);
+            assert_eq!(results[0].document, doc_b.clone());
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![1, 2, 4], bands: vec![300], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 2);
+            assert_eq!(results[0].document, doc_a.clone());
+            assert_eq!(results[1].document, doc_b.clone());
+        }
+        {
+            let mut backend = Stream::<String>::new("/tmp/stream_b", Params {
+                min_tree_height: 1,
+                max_block_size: 32,
+                memory_limit_power: 13,
+                parallel_config: ParallelConfig::SingleThread,
+            }).unwrap();
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![1, 2, 3], bands: vec![100, 400], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 1);
+            assert_eq!(results[0].similarity, 1.0);
+            assert_eq!(results[0].document, doc_a.clone());
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![4, 5, 6], bands: vec![200, 500, 600, 700], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 1);
+            assert_eq!(results[0].similarity, 1.0);
+            assert_eq!(results[0].document, doc_b.clone());
+            let results = backend.lookup(Arc::new(Signature { minhash: vec![1, 2, 4], bands: vec![300], }),
+                                         Box::new(SimilarityThresholdFilter(0.0)),
+                                         Vec::new()).unwrap();
+            assert_eq!(results.len(), 2);
+            assert_eq!(results[0].document, doc_a.clone());
+            assert_eq!(results[1].document, doc_b.clone());
+        }
     }
 }
