3d9016197c5282f6e112b977fef130a25e4ce9d5
Alexey Voznyuk <me@swizard.info>
2015-12-03 16:29:05 +0300

    implement "switching" state to give pile_lookup some time for database opening

--- src/backend/pile_rw.rs
+++ src/backend/pile_rw.rs
@@ -1,6 +1,7 @@
 use std::mem;
 use std::sync::Arc;
-use std::sync::mpsc::TryRecvError;
+use std::thread::spawn;
+use std::sync::mpsc::{channel, Receiver, TryRecvError};
 use std::path::{Path, PathBuf};
 use serde::{Serialize, Deserialize};
 use super::worker::{Worker, Req, Rep};
@@ -11,11 +12,12 @@ enum RunState<D> where D: Serialize + Deserialize + Send + Sync + 'static {
     Invalid,
     Filling { in_memory: Worker<in_memory::InMemory<D>, ()>, compile: Worker<pile_compile::PileCompile<D>, pile_compile::Error>, },
     Freezing { in_memory: Worker<in_memory::InMemory<D>, ()>, compile: Worker<pile_compile::PileCompile<D>, pile_compile::Error>, },
+    Switching { in_memory: Worker<in_memory::InMemory<D>, ()>, rx: Receiver<Result<pile_lookup::PileLookup<D>, pile_lookup::Error>>, },
     Freezed { lookup: pile_lookup::PileLookup<D>, },
 }
 
 pub struct PileRw<D> where D: Serialize + Deserialize + Send + Sync + 'static {
-    database_dir: PathBuf,
+    database_dir: Arc<PathBuf>,
     state: RunState<D>,
 }
 
@@ -35,7 +37,7 @@ impl<D> PileRw<D> where D: Serialize + Deserialize + Send + Sync + 'static {
         let in_memory = in_memory::InMemory::new();
         let compile = try!(pile_compile::PileCompile::new(&base_dir, compile_params).map_err(|e| Error::PileCompile(e)));
         Ok(PileRw {
-            database_dir: base_dir,
+            database_dir: Arc::new(base_dir),
             state: RunState::Filling {
                 in_memory: Worker::run(in_memory),
                 compile: Worker::run(compile),
@@ -62,6 +64,9 @@ impl<D> Drop for PileRw<D> where D: Serialize + Deserialize + Send + Sync + 'sta
                         break
                     }
                 },
+            RunState::Switching { rx: pile_rx, .. } => {
+                let _ = pile_rx.recv().unwrap();
+            },
         }
     }
 }
@@ -79,7 +84,7 @@ impl<D> Backend for PileRw<D> where D: Serialize + Deserialize + Send + Sync + '
                 try!(pile_backend.save_state(state).map_err(|e| Error::PileCompile(e)));
                 Ok(())
             },
-            RunState::Freezing { .. } | RunState::Freezed { .. } =>
+            RunState::Freezing { .. } | RunState::Switching { .. } | RunState::Freezed { .. } =>
                 Err(Error::UnsupportedForCurrentMode),
         }
     }
@@ -91,6 +96,8 @@ impl<D> Backend for PileRw<D> where D: Serialize + Deserialize + Send + Sync + '
             RunState::Filling { in_memory: ref mut backend, .. } =>
                 backend.load_state().map_err(|_| Error::InMemory),
             RunState::Freezing { in_memory: ref mut backend, .. } =>
+                backend.load_state().map_err(|_| Error::InMemory),
+            RunState::Switching { in_memory: ref mut backend, .. } =>
                 backend.load_state().map_err(|_| Error::InMemory),
             RunState::Freezed { lookup: ref mut backend } =>
                 backend.load_state().map_err(|e| Error::PileLookup(e)),
@@ -111,7 +118,7 @@ impl<D> Backend for PileRw<D> where D: Serialize + Deserialize + Send + Sync + '
                     other @ (Ok(..), Ok(..)) => panic!("unexpected rep pair: {:?}", other),
                 }
             },
-            RunState::Freezing { .. } | RunState::Freezed { .. } =>
+            RunState::Freezing { .. } | RunState::Switching { .. } | RunState::Freezed { .. } =>
                 Err(Error::UnsupportedForCurrentMode),
         }
     }
@@ -119,15 +126,20 @@ impl<D> Backend for PileRw<D> where D: Serialize + Deserialize + Send + Sync + '
     fn lookup<F, C, CR, CE>(&mut self, signature: Arc<Signature>, filter: F, collector: C) -> Result<CR, LookupError<Error, CE>>
         where F: CandidatesFilter + Clone, C: CandidatesCollector<Error = CE, Document = D, Result = CR>
     {
+        // check if freezing is done
         if let RunState::Freezing { .. } = self.state {
-            if let RunState::Freezing { in_memory: mem_backend, compile: mut pile_compile } = mem::replace(&mut self.state, RunState::Invalid) {
+            if let RunState::Freezing { in_memory: mem_backend, compile: mut pile_compile, } = mem::replace(&mut self.state, RunState::Invalid) {
                 loop {
                     match pile_compile.rx.try_recv() {
                         Ok(Ok(Rep::TerminateAck)) => {
                             pile_compile.shutdown();
-                            let pile_lookup =
-                                try!(pile_lookup::PileLookup::new(&self.database_dir).map_err(|e| LookupError::Backend(Error::PileLookup(e))));
-                            self.state = RunState::Freezed { lookup: pile_lookup, };
+                            let database_dir_copy = self.database_dir.clone();
+                            let (tx, rx) = channel();
+                            spawn(move || {
+                                mem::drop(pile_compile);
+                                tx.send(pile_lookup::PileLookup::new(&**database_dir_copy)).unwrap();
+                            });
+                            self.state = RunState::Switching { in_memory: mem_backend, rx: rx, };
                         },
                         Ok(..) =>
                             continue,
@@ -141,6 +153,23 @@ impl<D> Backend for PileRw<D> where D: Serialize + Deserialize + Send + Sync + '
             }
         }
 
+        // check if switching is done
+        if let RunState::Switching { .. } = self.state {
+            if let RunState::Switching { in_memory: mem_backend, rx: pile_rx, } = mem::replace(&mut self.state, RunState::Invalid) {
+                match pile_rx.try_recv() {
+                    Ok(Ok(pile_lookup)) =>
+                        self.state = RunState::Freezed { lookup: pile_lookup, },
+                    Ok(Err(e)) =>
+                        return Err(LookupError::Backend(Error::PileLookup(e))),
+                    Err(TryRecvError::Empty) =>
+                        self.state = RunState::Switching { in_memory: mem_backend, rx: pile_rx, },
+                    Err(..) =>
+                        panic!("switching thread died unexpectedly"),
+                }
+            }
+        }
+
+        // process lookup
         match self.state {
             RunState::Invalid =>
                 unreachable!(),
@@ -150,6 +179,11 @@ impl<D> Backend for PileRw<D> where D: Serialize + Deserialize + Send + Sync + '
                     LookupError::Backend(..) => LookupError::Backend(Error::InMemory),
                 }),
             RunState::Freezing { in_memory: ref mut backend, .. } =>
+                backend.lookup(signature, filter, collector).map_err(|err| match err {
+                    LookupError::Collector(e) => LookupError::Collector(e),
+                    LookupError::Backend(..) => LookupError::Backend(Error::InMemory),
+                }),
+            RunState::Switching { in_memory: ref mut backend, .. } =>
                 backend.lookup(signature, filter, collector).map_err(|err| match err {
                     LookupError::Collector(e) => LookupError::Collector(e),
                     LookupError::Backend(..) => LookupError::Backend(Error::InMemory),
@@ -172,6 +206,10 @@ impl<D> Backend for PileRw<D> where D: Serialize + Deserialize + Send + Sync + '
                 Ok(())
             },
             state @ RunState::Freezing { .. } => {
+                self.state = state;
+                Err(Error::UnsupportedForCurrentMode)
+            },
+            state @ RunState::Switching { .. } => {
                 self.state = state;
                 Err(Error::UnsupportedForCurrentMode)
             },
